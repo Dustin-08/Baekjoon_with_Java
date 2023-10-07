15f80f639c83b859e3b59e556f34820db2bae55e
Dustin-08 <cjhjik1@gmail.com>
2023-10-07 19:01:03 +0900

    자바 1단계 자릿수, 정민 추가

---
+++ Step_1/jeogmin.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class jeogmin {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        long a = in.nextLong();
+        long b = in.nextLong();
+        long c = in.nextLong();
+
+        System.out.println(a+b+c);
+        in.close();
+    }
+}
